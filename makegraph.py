a452e881561906801d29da6c1e0ce9b9c9e8e551
hmrm <hmrmaxwell@gmail.com>
2012-09-14 18:20:37 -0700

    results are terrible. Throwing this one away

--- makegraph.py
+++ makegraph.py
@@ -16,56 +16,70 @@ sys.stderr.write("Building Recommendation Graph\n")
 
 dg = digraph()
 dg.add_nodes([x.strip() for x in nodes])
-
-for node in nodes:
-    edges = open(nodeedgedir + "/" + node.strip()).readlines()
-    for edge in edges:
-        edge = edge.strip().split()
-        try:
-            dg.add_edge((node.strip(), edge[0]), wt=int(edge[2]))
-            dg.add_node_attribute(edge[0], ("label", edge[1]))
-        except AdditionError:
-            pass
-
-sys.stderr.write("Normalizing Weights\n")
-for node in dg:
-    totalweight = 0
-    for neighbor in dg.neighbors(node):
-        totalweight += dg.edge_weight(node, neighbor)
-    for neighbor in dg.neighbors(node):
-        try:
-            dg.set_edge_weight(node, neighbor, dg.edge_weight(node, neighbor) / totalweight)
-        except ZeroDivisionError:
-            pass
-
-sys.stderr.write("Setting Initial Values\n")
-for line in userdata:
-    id_rating = line.strip().split()
-    if id_rating[0] in dg:
-        dg.add_node_attribute(id_rating[0], ("rating", int(id_rating[1])))
-        dg.add_node_attribute(id_rating[0], ("initial", True))
-        dg.add_node_attribute(id_rating[0], ("rated", True))
-
-sys.stderr.write("Iterating\n")
-for i in xrange(int(iterations)):
+try:
+    for node in nodes:
+        edges = open(nodeedgedir + "/" + node.strip()).readlines()
+        for edge in edges:
+            edge = edge.strip().split()
+            try:
+                dg.add_edge((node.strip(), edge[0]), wt=int(edge[2]))
+                dg.add_node_attribute(edge[0], ("label", edge[1]))
+            except AdditionError:
+                pass
+    
+    sys.stderr.write("Normalizing Weights\n")
     for node in dg:
-        if not ("initial", True) in dg.node_attributes(node):
-            totalincidentweight = 0
-            totalincidentvalues = 0
-            for incident in dg.incidents(node):
-                if "rating" in [x[0] for x in dg.node_attributes(incident)]:
-                    totalincidentweight += dg.edge_weight(incident, node)
-                    totalincidentvalues += dg.edge_weight(incident, node) * [x[1] for x in dg.node_attributes(incident) if x[0] == "rating"][0]
-            if totalincidentweight > 0:
-                dg.add_node_attribute(node, ("newrating", totalincidentvalues / totalincidentweight))
-                dg.add_node_attribute(node, ("update", True))
-    for node in dg:
-        if ("update", True) in dg.node_attributes(node):
-            dg.add_node_attribute(node, ("rating", [x[1] for x in dg.node_attributes(incident) if x[0] == "newrating"][0]))
-            dg.add_node_attribute(node, ("update", False))
-            dg.add_node_attribute(node, ("rated", True))
+        totalweight = 0
+        for neighbor in dg.neighbors(node):
+            totalweight += dg.edge_weight((node, neighbor))
+        for neighbor in dg.neighbors(node):
+            try:
+                dg.set_edge_weight((node, neighbor), dg.edge_weight((node, neighbor)) / totalweight)
+            except ZeroDivisionError:
+                pass
+    
+    sys.stderr.write("Setting Initial Values\n")
+    for line in userdata:
+        id_rating = line.strip().split()
+        try:
+            if id_rating[0] in dg:
+                dg.add_node_attribute(id_rating[0], ("rating", int(id_rating[1])))
+                dg.add_node_attribute(id_rating[0], ("initial", True))
+                dg.add_node_attribute(id_rating[0], ("rated", True))
+        except IndexError:
+            pass
+    
+    sys.stderr.write("Iterating\n")
+    for i in xrange(int(iterations)):
+        sys.stderr.write("Iteration number " + str(i + 1) + "\n")
+        newinfo = False
+        for node in dg:
+            if not ("initial", True) in dg.node_attributes(node):
+                totalincidentweight = 0
+                totalincidentvalues = 0
+                for incident in dg.incidents(node):
+                    if "rating" in [x[0] for x in dg.node_attributes(incident)]:
+                        totalincidentweight += dg.edge_weight((incident, node))
+                        totalincidentvalues += dg.edge_weight((incident, node)) * [x[1] for x in dg.node_attributes(incident) if x[0] == "rating"][0]
+                if totalincidentweight > 0:
+                    dg.add_node_attribute(node, ("newrating", totalincidentvalues / totalincidentweight))
+                    dg.add_node_attribute(node, ("update", True))
+        for node in dg:
+            if ("update", True) in dg.node_attributes(node):
+                if not ("rated", True) in dg.node_attributes(node):
+                    newinfo = True
+                dg.add_node_attribute(node, ("rating", [x[1] for x in dg.node_attributes(incident) if x[0] == "newrating"][0]))
+                dg.add_node_attribute(node, ("update", False))
+                dg.add_node_attribute(node, ("rated", True))
+        sys.stderr.write("New information? :" + str(newinfo) + "\n")
+        if not newinfo:
+            break
+        
+except IndexError:
+    print 5
+    exit(0)
 
-sys.stderr.write("Reading Prediction")
+sys.stderr.write("Reading Prediction\n")
 if not predictionid in [node for node in dg if ("rated", True) in dg.node_attributes(node)]:
     print 5
 else:
--- testmodel.sh
+++ testmodel.sh
@@ -2,7 +2,9 @@
 
 #first is the training, second is the validation, third is the iterations, fourth is the nodefile, fifth is the datadir
 
-for i in $(cat $1 | wc -l)
+for i in $(seq 1 $(cat $1 | wc -l))
 do
-   awk 'NR=='$i $1 | tr "," "\n" | python makegraph.py $4 $5 $(awk 'NR=='$i'{print $1}' $2) $3
+    awk 'NR=='$i $2 1>&2
+    echo $i"-th test" 1>&2
+    awk 'NR=='$i $1 | tr "," "\n" | python makegraph.py $4 $5 $(awk 'NR=='$i'{print $1}' $2) $3
 done
